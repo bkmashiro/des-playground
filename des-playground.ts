8852a5c79c394ebdda0f55b19e577c806ad24548
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-04-06 13:41:53 +0800

    update text

--- des-playground.ts
+++ des-playground.ts
@@ -1,10 +1,11 @@
 import { Bits } from "./backend";
 import { SimpleDES } from "./cipher/DES/simple-des";
 import { decodeLengthedString, encodeLengthedString } from "./cipher/LengthedString/LengthedString";
+import { ECB } from "./cipher/ModeWarppers/ECB";
 
-const key = Bits.fromNumber(0b1010000010, 10)
-const des = new SimpleDES(key);
-const text = 'hello';
+const key = Bits.fromNumber(0b1100011110, 10)
+const des = new ECB(new SimpleDES(key));
+const text = 'Network security encompasses all the steps taken to protect the integrity of a computer network and the data within it. Network security is important because it keeps sensitive data safe from cyber attacks and ensures the network is usable and trustworthy. Successful network security strategies employ multiple security solutions to protect users and organizations from malware and cyber attacks, like distributed denial of service.';
 console.log(`     text:`, text);
 const plaintext = Bits.fromU8Array(encodeLengthedString(text));
 console.log(`plaintext:`, plaintext.toBinaryString());
